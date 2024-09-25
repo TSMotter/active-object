0d6d8a0ec8dc7850934e21d9f47cf76811059267
Giuliano Motter <mottergiuliano@gmail.com>
2024-09-24 22:46:01 -0300

    [#3] Working 'toaster' example

--- samples/intermediate/main.cpp
+++ samples/intermediate/main.cpp
@@ -244,8 +244,8 @@ class ActorFoo
 };
 
 /* clang-format off */
-int ActorFooSuperState::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int ActorFooSuperState::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int ActorFooSuperState::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int ActorFooSuperState::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int ActorFooSuperState::process_event(IEvent_ptr event)
 {
     (void) event;
@@ -297,25 +297,25 @@ int StateA::process_event(IEvent_ptr event)
     }
     return -1;  // Unhandled event
 }
-int StateA::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int StateC::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int StateC::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int StateA::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int StateC::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int StateC::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int StateC::process_event(IEvent_ptr event)
 {
     (void) event;
     LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
     return 0;
 }
-int StateD::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int StateD::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int StateD::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int StateD::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int StateD::process_event(IEvent_ptr event)
 {
     (void) event;
     LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
     return 0;
 }
-int StateF::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int StateF::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int StateF::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int StateF::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int StateF::process_event(IEvent_ptr event)
 {
     (void) event;
@@ -332,7 +332,7 @@ int StateG::on_entry()
     m_actor->m_signal(event);
     return 0;
 }
-int StateG::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int StateG::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int StateG::process_event(IEvent_ptr event)
 {
     LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
@@ -567,8 +567,8 @@ class ActorBar
 };
 
 /* clang-format off */
-int ActorBarSuperState::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
-int ActorBarSuperState::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; return 0; }
+int ActorBarSuperState::on_entry() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
+int ActorBarSuperState::on_exit() { LOG_MAIN << __PRETTY_FUNCTION__ << std::endl; return 0; }
 int ActorBarSuperState::process_event(IEvent_ptr event)
 {
     (void) event;
--- samples/toaster/main.cpp
+++ samples/toaster/main.cpp
@@ -17,20 +17,34 @@
 namespace Evts
 {
 
-class EventBlue : public IEvent
+class DoorOpen : public IEvent
+{
+};
+class DoorClose : public IEvent
+{
+};
+class DoToasting : public IEvent
 {
    public:
-    int timeout;
+    // DoToasting() : timeout{60000}
+    //{
+    // }
+    uint64_t timeout;
 };
-class EventGreen : public IEvent
+class DoBaking : public IEvent
 {
    public:
-    std::vector<int> data;
+    // DoBaking() : temperature{140}
+    //{
+    // }
+    float temperature;
 };
-class EventShutdown : public IEvent
+class Timeout : public IEvent
 {
 };
-
+class Shutdown : public IEvent
+{
+};
 }  // namespace Evts
 
 /* ============================================================================================== */
@@ -67,8 +81,7 @@ class Heating : public ToasterSuperState
 {
    public:
     virtual ~Heating() = default;
-    Heating(Toaster* actor)
-        : ToasterSuperState(actor)
+    Heating(Toaster* actor) : ToasterSuperState(actor)
     {
     }
     virtual int on_entry() override;
@@ -79,8 +92,7 @@ class DoorOpen : public ToasterSuperState
 {
    public:
     virtual ~DoorOpen() = default;
-    DoorOpen(Toaster* actor)
-        : ToasterSuperState(actor)
+    DoorOpen(Toaster* actor) : ToasterSuperState(actor)
     {
     }
     virtual int on_entry() override;
@@ -91,8 +103,7 @@ class Toasting : public Heating
 {
    public:
     virtual ~Toasting() = default;
-    Toasting(Toaster* actor)
-        : Heating(actor)
+    Toasting(Toaster* actor) : Heating(actor)
     {
     }
     virtual int on_entry() override;
@@ -103,8 +114,7 @@ class Baking : public Heating
 {
    public:
     virtual ~Baking() = default;
-    Baking(Toaster* actor)
-        : Heating(actor)
+    Baking(Toaster* actor) : Heating(actor)
     {
     }
     virtual int on_entry() override;
@@ -116,20 +126,287 @@ class Toaster
 {
    public:
     using ToasterSuperState_ptr = std::shared_ptr<ToasterSuperState>;
+
     Toaster() : m_queue{std::make_shared<SimplestThreadSafeQueue<IEvent_ptr>>()}
     {
+        tree<ToasterSuperState_ptr> tree;
+        tree.set_head(std::make_shared<ToasterSuperState>(this));
+        m_states[StateValue::ROOT] = tree.begin();
+        m_states[StateValue::HEATING] =
+            tree.append_child(m_states[StateValue::ROOT], std::make_shared<Heating>(this));
+        m_states[StateValue::DOOR_OPEN] =
+            tree.append_child(m_states[StateValue::ROOT], std::make_shared<DoorOpen>(this));
+        m_states[StateValue::TOASTING] =
+            tree.append_child(m_states[StateValue::HEATING], std::make_shared<Toasting>(this));
+        m_states[StateValue::BAKING] =
+            tree.append_child(m_states[StateValue::HEATING], std::make_shared<Baking>(this));
+        m_states[StateValue::UNKNOWN] = tree.end();
 
+        m_next_state = m_states[StateValue::UNKNOWN];
+
+        m_state_manager = std::make_shared<StateManager<ToasterSuperState_ptr>>(
+            std::move(tree), m_states[StateValue::HEATING]);
     }
     ~Toaster()
     {
     }
+
+    void start()
+    {
+        m_state_manager->init();
+        m_running = true;
+        m_thread  = std::thread(&Toaster::run, this);
+    }
+
+    void stop()
+    {
+        if (!m_running)
+            return;
+
+        m_running = false;
+        m_queue->clear();
+
+        if (m_thread.joinable())
+            m_thread.join();
+    };
+
+    void callback_IEvent(IEvent_ptr event)
+    {
+        m_queue->put(event);
+    }
+
+    template <class T>
+    connection connect_callbacks(T&& handler)
+    {
+        return m_signal.connect(handler);
+    }
+
+    void heater_on()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void heater_off()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void arm_time_event()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void disarm_time_event()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void set_temperature()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void reset_temperature()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void internal_lamp_on()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    void internal_lamp_off()
+    {
+        LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    }
+    void run_once()
+    {
+        m_running = false;
+        run();
+    }
+
+    SignalIEvent                                                m_signal;
+    std::shared_ptr<StateManager<ToasterSuperState_ptr>>        m_state_manager;
+    std::map<StateValue, tree<ToasterSuperState_ptr>::iterator> m_states;
+    tree<ToasterSuperState_ptr>::iterator                       m_next_state;
+
+   private:
+    void run()
+    {
+        do
+        {
+            IEvent_ptr current_event = m_queue->wait_and_pop();
+            m_state_manager->processEvent(current_event);
+
+            if (m_next_state != m_states[StateValue::UNKNOWN])
+            {
+                m_state_manager->transitionTo(m_next_state);
+                m_next_state = m_states[StateValue::UNKNOWN];
+            }
+
+        } while (m_running);
+    };
+
+    std::atomic_bool                                     m_running{false};
+    std::thread                                          m_thread;
+    std::shared_ptr<SimplestThreadSafeQueue<IEvent_ptr>> m_queue;
 };
 
+int ToasterSuperState::on_entry()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    return 0;
 }
+int ToasterSuperState::on_exit()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    return 0;
+}
+int ToasterSuperState::process_event(IEvent_ptr event)
+{
+    (void) event;
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    return 0;
+}
+int Heating::on_entry()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->heater_on();
+    return 0;
+}
+int Heating::on_exit()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->heater_off();
+    return 0;
+}
+int Heating::process_event(IEvent_ptr event)
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+
+    std::size_t event_type = event->getTypeHash();
+    if (event_type == typeid(Evts::DoorOpen).hash_code())
+    {
+        m_actor->m_next_state = m_actor->m_states[StateValue::DOOR_OPEN];
+        return 0;
+    }
+    else if (event_type == typeid(Evts::DoToasting).hash_code())
+    {
+        m_actor->m_next_state = m_actor->m_states[StateValue::TOASTING];
+        return 0;
+    }
+    else if (event_type == typeid(Evts::DoBaking).hash_code())
+    {
+        m_actor->m_next_state = m_actor->m_states[StateValue::BAKING];
+        return 0;
+    }
+    return -1;  // unhandled event
+}
+int DoorOpen::on_entry()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->internal_lamp_on();
+    return 0;
+}
+int DoorOpen::on_exit()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->internal_lamp_off();
+    return 0;
+}
+int DoorOpen::process_event(IEvent_ptr event)
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+
+    std::size_t event_type = event->getTypeHash();
+    if (event_type == typeid(Evts::DoorClose).hash_code())
+    {
+        m_actor->m_next_state = m_actor->m_states[StateValue::HEATING];
+        return 0;
+    }
+    return -1;  // unhandled event
+}
+int Toasting::on_entry()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->arm_time_event();
+    return 0;
+}
+int Toasting::on_exit()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->disarm_time_event();
+    return 0;
+}
+int Toasting::process_event(IEvent_ptr event)
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+
+    std::size_t event_type = event->getTypeHash();
+    if (event_type == typeid(Evts::Timeout).hash_code())
+    {
+        m_actor->m_next_state = m_actor->m_states[StateValue::HEATING];
+        return 0;
+    }
+    return -1;  // unhandled event
+}
+int Baking::on_entry()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->set_temperature();
+    return 0;
+}
+int Baking::on_exit()
+{
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    m_actor->reset_temperature();
+    return 0;
+}
+int Baking::process_event(IEvent_ptr event)
+{
+    (void) event;
+    LOG_MAIN << __PRETTY_FUNCTION__ << std::endl;
+    return 0;
+}
+}  // namespace Toaster
 
 /* ============================================================================================== */
 
 int main(int argc, char** argv)
 {
+    std::vector<IEvent_ptr> events;
+    events.emplace_back(std::make_shared<Evts::DoorOpen>()); // 0
+    events.emplace_back(std::make_shared<Evts::DoorClose>()); // 1
+    events.emplace_back(std::make_shared<Evts::DoToasting>()); // 2
+    events.emplace_back(std::make_shared<Evts::DoBaking>()); // 3
+    events.emplace_back(std::make_shared<Evts::Timeout>()); // 4
+    events.emplace_back(std::make_shared<Evts::Shutdown>()); // 5
 
+    std::shared_ptr<Toaster::Toaster> tst = std::make_shared<Toaster::Toaster>();
+    // tst->connect_callbacks();
+    // tst->start();
+    tst->m_state_manager->init();
+
+    int choice;
+    while (true)
+    {
+        std::cout << "Enter event number: ";
+        std::cin >> choice;
+
+        if (choice == -1)
+            break;
+
+        if (choice >= 0 && choice < events.size())
+        {
+            tst->callback_IEvent(events[choice]);
+            tst->run_once();
+        }
+        else
+        {
+            std::cout << "Invalid choice. Try again.\n";
+        }
+    }
+
+    return 0;
 }
